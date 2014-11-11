2081a86af3254b45e5844f280c8c97ce39708c6f
Francesca Barbazeni <francesca.barbazeni@zenointelligence.com>
2014-11-11 17:18:59 +0100

    release 2.0 + stripe

--- vendor/stripe/stripe_key.js
+++ vendor/stripe/stripe_key.js
@@ -1,2 +1,2 @@
-// This identifies your website in the createToken call below
+// This identifies your website in the createToken call below         // pk_live_4VeRpZ1a1ZeWD1Ca3Bw34ge2
 Stripe.setPublishableKey('pk_test_4VeRHwIpRhng8X5QgpobF1tB');

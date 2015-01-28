5a2233c0868bf58c2153ad5a5f1e6d933ce39469
Francesca Barbazeni <francesca.barbazeni@zenointelligence.com>
2015-01-28 17:48:20 +0100

    bug

--- app/views/check-document-code.js
+++ app/views/check-document-code.js
@@ -22,7 +22,7 @@ export default Ember.View.extend({
                 type: 'PUT',
                 data: data,
                 success: function(response) {
-                    if ( response.success === 'false' ){
+                    if ( !response.success ){
                         controller.set('name', null);
 
                         if( self.val === '' || self.val === null ){
